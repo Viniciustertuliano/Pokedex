1869a3e34a5b53b754d579411e0189296b8bd5c5
Vinicius Tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2021-08-03 00:50:59 -0300

    Pokedex project started

---
+++ src/main/java/com/pokedex/reactiveweb/ReactivewebApplication.java
@@ -0,0 +1,12 @@
+package com.pokedex.reactiveweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivewebApplication.class, args);
+	}
+}
